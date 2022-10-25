179dc133d4adcd16dd7c3c9052e7ec6507933c0d
Dennis Lövkvist <dlovkvist@im.se>
2022-10-25 11:02:35 +0200

    Moved cell count and stride to variables.

--- main.dpr
+++ main.dpr
@@ -7,13 +7,13 @@ uses System.SysUtils;
 
 {$APPTYPE CONSOLE}
 
-function WithinBounds(x:Integer): Integer;
+function WithinBounds(x:Integer;cell_count:Integer): Integer;
 begin
   if x < 0 then
     begin
       WithinBounds := 0;
     end
-  else if x > 399 then
+  else if x >= cell_count then
     begin
       WithinBounds := 0;
     end
@@ -22,9 +22,9 @@ begin
 end;
 
 
-procedure Step(var cells:Array of Integer; var cells_next:Array of Integer);
+procedure Step(var cells:Array of Integer; var cells_next:Array of Integer; cell_count:Integer;stride:Integer);
 
-const kernel:Array[0..8] of Integer = (-20-1,-20,-20+1,-1,0,1,20-1,20,20+1);
+var kernel:Array[0..8] of Integer;
 var i:Integer;
 var j:Integer;
 var ki:Integer;
@@ -32,7 +32,17 @@ var wb:Integer;
 var n_count:Integer;
 
 begin
-  for i := 0 to 399 do  
+  kernel[0] := -stride-1;
+  kernel[1] := -stride;
+  kernel[2] := -stride+1;
+  kernel[3] := -1;
+  kernel[4] := 0;
+  kernel[5] := 1;
+  kernel[6] := stride-1;
+  kernel[7] := stride;
+  kernel[8] := stride+1;
+
+  for i := 0 to cell_count do  
   begin 
     n_count := 0;
     //---Count neighbours---
@@ -41,7 +51,7 @@ begin
       ki:= i + kernel[j];
       if ki <> i then
       begin
-        wb := WithinBounds(ki); 
+        wb := WithinBounds(ki,cell_count); 
         if wb = 1 then
         begin
           if cells[ki] = 1 then
@@ -71,7 +81,7 @@ begin
   end;//End loop 0..399
 
   //---Update cells to the alterd state---//
-  for i := 0 to 399 do  
+  for i := 0 to cell_count do  
   begin
     cells[i] := cells_next[i];
   end;
@@ -80,7 +90,7 @@ begin
 end;
 
 
-procedure PrintBuffered(cells:Array of Integer);
+procedure PrintBuffered(cells:Array of Integer; cell_count:Integer; stride:Integer);
 var i:Integer;
 var n:Integer;
 var buffer:WideString;
@@ -88,7 +98,7 @@ begin
 
   n := 0;
   //---Loop cells---
-  for i := 0 to 399 do  
+  for i := 0 to cell_count do  
   begin 
     //---Print blakspace if cell = 0 and # if cell = 1---
     if cells[i] = 0 then
@@ -104,7 +114,7 @@ begin
 
     //---Print line break---
     n:=n+1;   
-    if n = 20 then
+    if n = stride then
     begin
       buffer := buffer + #13#10;
       n:=0;
@@ -117,13 +127,13 @@ begin
   WriteLn(buffer);
 end;
 
-procedure PrintImmediate(cells:Array of Integer);
+procedure PrintImmediate(cells:Array of Integer;cell_count:Integer; stride:Integer);
 var i:Integer;
 var n:Integer;
 begin
   n := 0;
   //---Loop cells---
-  for i := 0 to 399 do  
+  for i := 0 to cell_count do  
   begin 
     //---Print blakspace if cell = 0 and # if cell = 1---
     if cells[i] = 0 then
@@ -139,7 +149,7 @@ begin
 
     //---Print line break---
     n:=n+1;   
-    if n = 20 then
+    if n = stride then
     begin
       WriteLn(' ');
       n:=0;
@@ -152,12 +162,15 @@ end;
 
 
 //main-------------------------
+var stride:Integer = 20;
 var cells : array[0..399] of Integer;
 var cells_next : array[0..399] of Integer;
+var cell_count:Integer = 399;
 var i:Integer;
 var generation:Integer;
 begin
-  for i := 0 to 399 do  
+generation := 0;
+  for i := 0 to cell_count do  
     begin
     if Random(100) > 50 then
       begin
@@ -175,9 +188,9 @@ begin
   while true do
   begin
     
-  Step(cells,cells_next);
+  Step(cells,cells_next,cell_count,stride);
   generation := generation + 1;
-  PrintBuffered(cells);
+  PrintBuffered(cells,cell_count,stride);
   Write('_____________ ');  
   Write('Generation ');  
   Write(generation);  
